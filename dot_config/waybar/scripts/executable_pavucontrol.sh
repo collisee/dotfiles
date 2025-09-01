1b695b1de4f61aae5d1b213c00e1bb0d17c73390
collisee <136151261+collisee@users.noreply.github.com>
2025-09-01 13:37:37 +0700

    slight edits

--- dot_config/waybar/scripts/executable_pavucontrol.sh
+++ dot_config/waybar/scripts/executable_pavucontrol.sh
@@ -3,11 +3,13 @@
 WIDTH=720
 HEIGHT=720
 
+RESERVED_TOP=$(hyprctl -j monitors | jq '.[0].reserved[1]' 2>/dev/null)
+
 cursor_pos=$(hyprctl cursorpos)
 x=$(echo "$cursor_pos" | awk -F', ' '{print $1}')
 y=$(echo "$cursor_pos" | awk -F', ' '{print $2}')
 x=$(( x - WIDTH / 2 ))
 
-[ "$y" -lt 44 ] && y=44
+[ "$y" -lt "$RESERVED_TOP" ] && y=$RESERVED_TOP
 
-hyprctl dispatch "exec [noanim; float; move $x $y; size $WIDTH $HEIGHT] pavucontrol -t 3"
+hyprctl dispatch "exec [float; move $x $y; size $WIDTH $HEIGHT] pavucontrol -t 3"
